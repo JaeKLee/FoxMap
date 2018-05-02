7879ee26d2f725f7c714cf22f1f838187bb75975
miron0118 <leejae0118@gmail.com>
2018-05-02 16:23:15 +0000

    Added funtion testInput

--- PHP/adminLogin.php
+++ PHP/adminLogin.php
@@ -4,7 +4,8 @@
     <meta charset = "utf-8"/>
     <link rel="shortcut icon" href="..\pictures\seal.ico"/>
     <title>Edit</title>
-    <link rel="stylesheet" href="..\CSS\style.css">
+    <!--<link rel="stylesheet" href="..\css_marist\style.css">-->
+    <link rel="stylesheet" href="..\CSS\loginStyle.css">
 </head>
     
 <body>
@@ -26,9 +27,9 @@
             $userNameErr = "Please enter an username.";
         }
         else {
-            $userName = test_input($_POST["userName"]);
+            $userName = testInput($_POST["userName"]);
             // Checking to see if it contains letters only
-            if (!preg_match("/([a-zA-Z])\w+/g",$userName)) {
+            if (!preg_match("/([a-zA-Z])\w+/",$userName)) {
             $userNameErr = "Please enter letters only.";
             }
         }
@@ -38,7 +39,7 @@
             $emailErr = "Please enter an email.";
         }
         else {
-            $email = test_input($_POST["email"]);
+            $email = testInput($_POST["email"]);
             // Checking to see if it contains letters only
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $emailErr = "Please enter letters only.";
@@ -50,7 +51,7 @@
             $passErr = "Please enter a password.";
         }
         else {
-            $pass = test_input($_POST["password"]);
+            $pass = testInput($_POST["password"]);
             // Checking to see if it contains letters only
             if (!preg_match("/([a-zA-Z])\w+/g",$pass)) {
             $passErr = "Please enter letters only.";
@@ -58,17 +59,24 @@
         }
     }
     
+    function testInput($input) {
+        $input = trim($input);
+        $input = stripslashes($input);
+        $input = htmlspecialchars($input);
+        return $input;
+    }
     ?>
     
     <h2>TEST</h2>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-
-        Name: <input type="text" name="userName" placeholder="Name" style=" " value="<?php echo $userName;?>">
-        <span style="color:red;">* <?php echo $userNameErr;?></span>
+        <div id="loginForm">
+            Login <span style="color:red;">* <?php echo $userNameErr;?></span>
+            <input type="text" name="userName" placeholder="Name" value="<?php echo $userName;?>">
+        </div>
     </form>
     
-
     
+
 
 </body>
     
